14e6f2f228e45012ba5f31fb120a68053329a153
smajeji86 <62031784+smajeji86@users.noreply.github.com>
2021-05-24 20:42:20 +0530

    Delete goto.py

--- goto.py
+++
@@ -1,9 +0,0 @@
-from dronekit import connect,VehicleMode,LocationGlobalRelative,LocationGlobal
-import time
-vehicle=connect("udp:127.0.0.1:14550",wait_ready=True)
-
-vehicle.mode = VehicleMode("GUIDED")
-
-# Set the target location in global-relative frame
-a_location = LocationGlobalRelative(-34.364114, 149.166022, 30)
-vehicle.simple_goto(a_location)
